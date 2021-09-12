100aaa1f86548eea05db3e8b5d5f7908235803a8
Renan Blasechi <renan@fasters.com.br>
2021-09-12 10:18:27 -0300

    Interpolacao, Ajuste e diretivas Angular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,17 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any ;
+  public eventos: any = [];
+  public showImage: boolean = true;
+  public eventosFiltrados: any = [];
 
+  private _filtroList: string = "";
+
+  public get filtroList() {
+    return this._filtroList;
+  }
+
+  public set filtroList(value: string) {
+     this._filtroList = value;
+     this.eventosFiltrados = this.filtroList ? this.FiltrarEventos(this.filtroList) : this.eventos;
+  }
+
+  FiltrarEventos(filter: string): any {
+    filter = filter.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filter) !== -1 || evento.local.toLocaleLowerCase().indexOf(filter) !== -1
+    )
+  }
+
+  widthImage: number = 150;
+  marginImage: number = 2;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getEventos();
   }
 
+  updateImage(){
+    this.showImage = !this.showImage;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
   }
